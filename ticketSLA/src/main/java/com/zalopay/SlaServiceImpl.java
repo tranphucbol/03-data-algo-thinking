b15233771a9ef4673e595a3302c50148ecf430b0
Phuc. Tran Trong (4) <phuctt4@vng.com.vn>
2019-07-05 13:32:32 +0700

    update

--- ticketSLA/src/main/java/com/zalopay/SlaServiceImpl.java
+++ ticketSLA/src/main/java/com/zalopay/SlaServiceImpl.java
@@ -19,14 +19,11 @@ public class SlaServiceImpl implements SlaService {
 
         Duration duration = null;
 
-        Duration gap_1 = Duration.ofMinutes(90); //1h30 (12h00 -> 13h30)
-        Duration gap_2 = Duration.ofMinutes(14 * 60 + 30);//14h30 (18h00 -> 8h30 tomorrow)
-        Duration gap_3 = Duration.ofHours(8);
+        Duration breakTime = Duration.ofMinutes(90); //1h30 (12h00 -> 13h30)
 
         LocalTime timeBegin = begin.toLocalTime();
         LocalTime timeEnd = end.toLocalTime();
 
-        int n = 0; //gap redundancy
 
         //reset time if
         //begin before 8h30
@@ -44,50 +41,52 @@ public class SlaServiceImpl implements SlaService {
         }
 
         if(begin.getDayOfWeek().compareTo(DayOfWeek.SATURDAY) == 0) {
-            begin = begin.plusDays(2);
-        } else if(begin.getDayOfWeek().compareTo(DayOfWeek.SUNDAY) == 0) {
-            begin = begin.plusDays(1);
+            if(timeBegin.compareTo(timeEndMor) > 0) {
+                begin = begin.plusDays(2);
+                begin = LocalDateTime.of(begin.toLocalDate(), timeBeginMor);
+            }
         }
 
-        //if time begin is 8h30 -> 12h00 and time end is after 13h30 -> n++
-        if (timeBegin.compareTo(timeEndMor) <= 0 && timeEnd.compareTo(timeBeginAft) >= 0) {
-            n++;
-        } else if (timeBegin.compareTo(timeBeginAft) >= 0 && timeBegin.compareTo(timeEndAft) <= 0 &&
-                timeEnd.compareTo(timeBeginMor) >= 0 && timeEnd.compareTo(timeEndMor) <= 0) {
-            n--;
+        if(begin.getDayOfWeek().compareTo(DayOfWeek.SUNDAY) == 0) {
+            begin = begin.plusDays(1);
+            begin = LocalDateTime.of(begin.toLocalDate(), timeBeginMor);
         }
 
         duration = Duration.between(begin, end);
 
         //response in day
         if (begin.toLocalDate().equals(end.toLocalDate())) {
-            duration = duration.minus(gap_1.multipliedBy(n));
+            if (isMorning(begin.toLocalTime()) && isAfternoon(end.toLocalTime())) {
+                duration = duration.minus(breakTime);
+            }
         } else {
-            //(01/01 9h30 -> 02/01 8h30) n++
-            //(01/01 14h30 -> 02/01 13h30) n++
-            //(01/01 9h30 -> 02/01 14h30) n++
-            n++;
-
             long days = duration.toDays();
-            long weekends =  (end.getDayOfYear() / 7 - begin.getDayOfYear() / 7) * 2;
+            if(days > 0 && duration.toMinutes() % (24 * 60) < 12 * 60) {
+                days--;
+            }
+            long weekends = (end.getDayOfYear() / 7 - begin.getDayOfYear() / 7);
+            duration = Duration.ofMinutes((days - weekends) * 8 * 60 - weekends * (60 * 4 + 30));
 
-            //different day -> days must be 1
-            //days = 0 because time < 24H, we need assign days to 1
-            if (days == 0) days = 1;
+            if(isMorning(begin.toLocalTime())) {
+                duration = duration.plus(Duration.between(begin.toLocalTime(), timeEndAft).minus(breakTime));
+            } else {
+                duration = duration.plus(Duration.between(begin.toLocalTime(), timeEndAft));
+            }
 
-            // duration =
-            // + duration(begin, end)
-            // - duration(12h00 -> 13h30) x (days - 1 + gap_redundancy)
-            // - duration(18h -> 8h30 tomorrow) x (days)
-            // - duration(8h (1 work day)) x (weekends)
-            duration = duration
-                    .minus(gap_1.multipliedBy(days - 1 + n))
-                    .minus(gap_2.multipliedBy(days))
-                    .minus(gap_3.multipliedBy(weekends));
-
+            if(isMorning(end.toLocalTime())) {
+                duration = duration.plus(Duration.between(timeBeginMor, end.toLocalTime()));
+            } else {
+                duration = duration.plus(Duration.between(timeBeginMor, end.toLocalTime()).minus(breakTime));
+            }
         }
 
         return duration;
     }
 
+    private boolean isMorning(LocalTime time) {
+        return (time.compareTo(timeBeginMor) >= 0 && time.compareTo(timeEndMor) <= 0);
+    }
+    private boolean isAfternoon(LocalTime time) {
+        return (time.compareTo(timeBeginAft) >= 0 && time.compareTo(timeEndAft) <= 0);
+    }
 }
--- ticketSLA/src/test/java/com/zalopay/SlaServiceTest.java
+++ ticketSLA/src/test/java/com/zalopay/SlaServiceTest.java
@@ -66,12 +66,55 @@ public class SlaServiceTest {
                 {
                         LocalDateTime.of(2019, 5, 5, 8, 30),
                         LocalDateTime.of(2019, 5, 6, 15, 30),
-                        Duration.ofMinutes(5 * 60 +  30) // 13h30m
+                        Duration.ofMinutes(5 * 60 + 30) // 13h30m
                 },
                 {
-                        LocalDateTime.of(2019, 4, 30, 19, 30),
-                        LocalDateTime.of(2019, 5, 1, 9, 30),
-                        Duration.ofMinutes(60) // 1h
+                        LocalDateTime.of(2019, 4, 30, 11, 30),
+                        LocalDateTime.of(2019, 4, 30, 11, 45),
+                        Duration.ofMinutes(15)
+                },
+                {
+                        LocalDateTime.of(2019, 4, 30, 15, 30),
+                        LocalDateTime.of(2019, 4, 30, 17, 45),
+                        Duration.ofMinutes(135)
+                },
+                {
+                        LocalDateTime.of(2019, 4, 30, 15, 00),
+                        LocalDateTime.of(2019, 5, 1, 9, 00),
+                        Duration.ofMinutes(210)
+                },
+                {
+                        LocalDateTime.of(2019, 4, 30, 15, 00),
+                        LocalDateTime.of(2019, 5, 3, 9, 00),
+                        Duration.ofMinutes(210 + 480 * 2)
+                },
+                {
+                        LocalDateTime.of(2019, 4, 30, 15, 00),
+                        LocalDateTime.of(2019, 5, 10, 9, 00),
+                        Duration.ofMinutes(210 + 480 * 9 - 270 - 480)
+                }
+                ,
+                {
+                        LocalDateTime.of(2019, 4, 30, 15, 00),
+                        LocalDateTime.of(2019, 5, 13, 9, 00),
+                        Duration.ofMinutes(210 + 480 * 12 - (270 + 480) * 2)
+                }
+                ,
+                {
+                        LocalDateTime.of(2019, 4, 30, 9, 00),
+                        LocalDateTime.of(2019, 5, 13, 15, 00),
+                        Duration.ofMinutes(750 + 480 * 12 - (270 + 480) * 2)
+                },
+                {
+                        LocalDateTime.of(2019, 4, 30, 9, 00),
+                        LocalDateTime.of(2019, 5, 13, 9, 00),
+                        Duration.ofMinutes(480 + 480 * 12 - (270 + 480) * 2)
+                }
+                ,
+                {
+                        LocalDateTime.of(2019, 4, 30, 9, 00),
+                        LocalDateTime.of(2019, 5, 13, 9, 30),
+                        Duration.ofMinutes(480 + 480 * 12 - (270 + 480) * 2 + 30)
                 }
         };
     }
